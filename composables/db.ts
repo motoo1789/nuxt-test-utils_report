5ec038122e9ad6cba6dd568997ed56d9c859ed37
motoo1789 <motoo1789@gmail.com>
2024-04-25 22:34:20 +0900

    add: readのテスト環境をサンプルで作成

--- composables/db.ts
+++ composables/db.ts
@@ -4,7 +4,7 @@ export async function insert(): Promise<any> {
     const prisma = new PrismaClient()
 
     const user = {
-        id : 'testtesttest0001',
+        id : 'testtesttest0002',
         name: 'insertテスト2',
         authorizer: 'testauthorizer02',
         mail:  'test2@test.net'
@@ -12,6 +12,24 @@ export async function insert(): Promise<any> {
 
     const createUser = await prisma.user.create({
         data: user
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+
+    console.log("スプリプト側:Prisma返却値")
+    console.log(createUser)    
+
+    return createUser;
+}
+
+export async function read(): Promise<any> {
+    const prisma = new PrismaClient()
+
+    const createUser = await prisma.user.findUnique({
+        where: {
+            id: "testtesttest0001"
+        }
     })
     .catch((error) => {
         console.error(error);
--- tests/composables/db.test.ts
+++ tests/composables/db.test.ts
@@ -1,4 +1,5 @@
 import { insert } from "../../composables/db.js"
+import { read } from "../../composables/db.js"
 import { describe, test, expect } from "vitest";
 
 describe("insert", () => {
@@ -8,5 +9,23 @@ describe("insert", () => {
         console.log(result);
         expect(result).not.toBeUndefined();
         
+    })
+})
+
+describe("read", () => {
+    test("readのテスト", async () => {
+
+        const test = {
+            id: 'testtesttest0001',
+            name: 'insertテスト2',
+            authorizer: 'testauthorizer02',
+            create_date: new Date('2024-04-21T09:24:46.028Z'),
+            mail: 'test2@test.net'
+        }
+        const result = await read();
+        console.log("テスト側:関数返却値")
+        console.log(result);
+        expect(test).toMatchObject(result)
+        // expect(result).not.toBeUndefined();
     })
 })
