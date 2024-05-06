f42b7662d42a90fd1f89d0cbdfee05bea859e943
motoo1789 <motoo1789@gmail.com>
2024-05-06 14:46:59 +0900

    add: checkout front
    - fetchがvitestで使えないのでテストはできていない

--- tests/pages/index.test.ts
+++ tests/pages/index.test.ts
@@ -3,12 +3,18 @@ import { mount } from "@vue/test-utils";
 import index from "../../pages/index.vue";
 
 describe("index.vue", () => {
-  test("貸出処理のフロントテスト", () => {
+  test("貸出処理のフロントテスト", async () => {
     const wrapper = mount(index);
     //console.log(wrapper.text());
     expect(wrapper.exists()).toBeTruthy();
 
     const card1 = wrapper.findComponent('.keyType1');
     expect(card1.exists()).toBeTruthy();
+
+    await card1.trigger('click');
+    // expect(clickresult).toBe("貸出処理完了です");
+
+    const alertSuccess = wrapper.findComponent('.checkoutAlert');
+    expect(alertSuccess.text()).toContain("貸出処理完了です");
   });
 });
